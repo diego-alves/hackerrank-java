4cecf81e3e589a4faae25cc6f1e2c9dba3fcbfcf
Diego Alves <dfqalves@gmail.com>
2016-07-09 17:07:31 -0300

    ignorefailtest

---
+++ src/test/java/dfqalves/hackerrank/IgnoreFailTest.java
@@ -0,0 +1,14 @@
+package dfqalves.hackerrank;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class IgnoreFailTest {
+
+    @Test
+    public void testIgnoreFail() {
+        assertTrue(false);
+    }
+
+
+}
